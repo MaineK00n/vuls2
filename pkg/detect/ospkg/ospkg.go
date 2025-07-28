467f2c79767c9cec3fb587da99c608c36a3b91c9
MaineK00n <mainek00n.1229@gmail.com>
2025-07-28 20:50:51 +0900

    feat(detect/ospkg): rename when kernel package (#226)
    * feat(detect/ospkg): rename when kernel package
    
    * docs: fix typo in comment
    
    Co-authored-by: Shunichi Shinohara <shino.shun@gmail.com>
    
    ---------
    
    Co-authored-by: Shunichi Shinohara <shino.shun@gmail.com>

--- pkg/detect/ospkg/ospkg.go
+++ pkg/detect/ospkg/ospkg.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"maps"
 	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -130,50 +131,87 @@ func convertVCQueryPackage(family ecosystemTypes.Ecosystem, p scanTypes.OSPackag
 		return sb.String(), nil
 	}
 
-	bn, err := pnfn(p.Name, p.ModularityLabel)
+	bn, bv, err := func() (string, string, error) {
+		bn, err := pnfn(p.Name, p.ModularityLabel)
+		if err != nil {
+			return "", "", errors.Wrap(err, "form binary package name")
+		}
+
+		bv, err := pvfn(p.Epoch, p.Version, p.Release)
+		if err != nil {
+			return "", "", errors.Wrap(err, "form binary package version")
+		}
+
+		return bn, bv, nil
+	}()
 	if err != nil {
-		return vcTypes.Query{}, errors.Wrap(err, "form binary package name")
+		return vcTypes.Query{}, errors.Wrap(err, "form binary package")
 	}
 
-	bv, err := pvfn(p.Epoch, p.Version, p.Release)
+	sn, sv, err := func() (string, string, error) {
+		switch family {
+		case ecosystemTypes.EcosystemTypeRedHat, ecosystemTypes.EcosytemCentOS:
+			// TODO: Theoretically, we should check non-emptiness of p.SrcVersion here too. However, for workaround
+			// in RHEL oval v2 -> vex transition, we accept empty SrcVersion and skip validation at the moment.
+			// This switch MUST BE removed soon.
+			if p.SrcName == "" {
+				return "", "", nil
+			}
+			sn, err := pnfn(p.SrcName, p.ModularityLabel)
+			if err != nil {
+				return "", "", errors.Wrap(err, "form source package name")
+			}
+
+			if p.SrcVersion == "" {
+				return sn, "", nil
+			}
+			sv, err := pvfn(p.SrcEpoch, p.SrcVersion, p.SrcRelease)
+			if err != nil {
+				return "", "", errors.Wrap(err, "form source package version")
+			}
+
+			return sn, sv, nil
+		case ecosystemTypes.EcosystemTypeDebian, ecosystemTypes.EcosystemTypeUbuntu:
+			if p.SrcName == "" || p.SrcVersion == "" {
+				return "", "", nil
+			}
+
+			n := p.SrcName
+			if isKernelPackage(family, p.SrcName) {
+				n = rename(family, p.SrcName)
+			}
+
+			sn, err := pnfn(n, "")
+			if err != nil {
+				return "", "", errors.Wrap(err, "form source package name")
+			}
+
+			sv, err := pvfn(p.SrcEpoch, p.SrcVersion, p.SrcRelease)
+			if err != nil {
+				return "", "", errors.Wrap(err, "form source package version")
+			}
+
+			return sn, sv, nil
+		default:
+			if p.SrcName == "" || p.SrcVersion == "" {
+				return "", "", nil
+			}
+
+			sn, err := pnfn(p.SrcName, p.ModularityLabel)
+			if err != nil {
+				return "", "", errors.Wrap(err, "form source package name")
+			}
+
+			sv, err := pvfn(p.SrcEpoch, p.SrcVersion, p.SrcRelease)
+			if err != nil {
+				return "", "", errors.Wrap(err, "form source package version")
+			}
+
+			return sn, sv, nil
+		}
+	}()
 	if err != nil {
-		return vcTypes.Query{}, errors.Wrap(err, "form binary package version")
-	}
-
-	var (
-		sn string
-		sv string
-	)
-	switch family {
-	case ecosystemTypes.EcosystemTypeRedHat, ecosystemTypes.EcosytemCentOS:
-		// TODO: Theoretically, we should check non-emptiess of p.SrcVersion here too. However, for workaround
-		// in RHEL oval v2 -> vex transition, we accept empty SrcVersion and skip validation at the moment.
-		// This switch MUST BE removed soon.
-		if p.SrcName != "" {
-			sn, err = pnfn(p.SrcName, p.ModularityLabel)
-			if err != nil {
-				return vcTypes.Query{}, errors.Wrap(err, "form source package name")
-			}
-
-			if p.SrcVersion != "" {
-				sv, err = pvfn(p.SrcEpoch, p.SrcVersion, p.SrcRelease)
-				if err != nil {
-					return vcTypes.Query{}, errors.Wrap(err, "form source package version")
-				}
-			}
-		}
-	default:
-		if p.SrcName != "" && p.SrcVersion != "" {
-			sn, err = pnfn(p.SrcName, p.ModularityLabel)
-			if err != nil {
-				return vcTypes.Query{}, errors.Wrap(err, "form source package name")
-			}
-
-			sv, err = pvfn(p.SrcEpoch, p.SrcVersion, p.SrcRelease)
-			if err != nil {
-				return vcTypes.Query{}, errors.Wrap(err, "form source package version")
-			}
-		}
+		return vcTypes.Query{}, errors.Wrap(err, "form source package")
 	}
 
 	return vcTypes.Query{
@@ -188,26 +226,195 @@ func convertVCQueryPackage(family ecosystemTypes.Ecosystem, p scanTypes.OSPackag
 			switch family {
 			case ecosystemTypes.EcosystemTypeRedHat, ecosystemTypes.EcosytemCentOS:
 				// TODO: This switch should also be removed soon as well as the above one.
-				if sn != "" {
-					return &vcTypes.QuerySource{
-						Family:     family,
-						Name:       sn,
-						Version:    sv,
-						Repository: p.Repository,
-					}
+				if sn == "" {
+					return nil
 				}
-				return nil
+				return &vcTypes.QuerySource{
+					Family:     family,
+					Name:       sn,
+					Version:    sv,
+					Repository: p.Repository,
+				}
 			default:
-				if sn != "" && sv != "" {
-					return &vcTypes.QuerySource{
-						Family:     family,
-						Name:       sn,
-						Version:    sv,
-						Repository: p.Repository,
-					}
+				if sn == "" || sv == "" {
+					return nil
 				}
-				return nil
+				return &vcTypes.QuerySource{
+					Family:     family,
+					Name:       sn,
+					Version:    sv,
+					Repository: p.Repository,
+				}
 			}
 		}(),
 	}, nil
+}
+
+func rename(family ecosystemTypes.Ecosystem, name string) string {
+	switch family {
+	case ecosystemTypes.EcosystemTypeDebian:
+		return strings.NewReplacer("linux-signed", "linux", "linux-latest", "linux", "-amd64", "", "-arm64", "", "-i386", "").Replace(name)
+	case ecosystemTypes.EcosystemTypeUbuntu:
+		return strings.NewReplacer("linux-signed", "linux", "linux-meta", "linux").Replace(name)
+	default:
+		return name
+	}
+}
+
+func isKernelPackage(family ecosystemTypes.Ecosystem, name string) bool {
+	switch family {
+	case ecosystemTypes.EcosystemTypeDebian:
+		switch ss := strings.Split(rename(family, name), "-"); len(ss) {
+		case 1:
+			return ss[0] == "linux"
+		case 2:
+			if ss[0] != "linux" {
+				return false
+			}
+			switch ss[1] {
+			case "grsec":
+				return true
+			default:
+				_, err := strconv.ParseFloat(ss[1], 64)
+				return err == nil
+			}
+		default:
+			return false
+		}
+	case ecosystemTypes.EcosystemTypeUbuntu:
+		switch ss := strings.Split(rename(family, name), "-"); len(ss) {
+		case 1:
+			return ss[0] == "linux"
+		case 2:
+			if ss[0] != "linux" {
+				return false
+			}
+			switch ss[1] {
+			case "armadaxp", "mako", "manta", "flo", "goldfish", "joule", "raspi", "raspi2", "snapdragon", "allwinner", "aws", "azure", "bluefield", "dell300x", "gcp", "gke", "gkeop", "ibm", "iot", "laptop", "lowlatency", "kvm", "nvidia", "oem", "oracle", "euclid", "hwe", "riscv", "starfive", "realtime", "mtk":
+				return true
+			default:
+				_, err := strconv.ParseFloat(ss[1], 64)
+				return err == nil
+			}
+		case 3:
+			if ss[0] != "linux" {
+				return false
+			}
+			switch ss[1] {
+			case "ti":
+				return ss[2] == "omap4"
+			case "raspi", "raspi2", "allwinner", "gke", "gkeop", "ibm", "oracle", "riscv", "starfive":
+				_, err := strconv.ParseFloat(ss[2], 64)
+				return err == nil
+			case "aws":
+				switch ss[2] {
+				case "hwe", "edge":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			case "azure":
+				switch ss[2] {
+				case "cvm", "fde", "edge":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			case "gcp":
+				switch ss[2] {
+				case "edge":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			case "intel":
+				switch ss[2] {
+				case "iotg", "opt":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			case "oem":
+				switch ss[2] {
+				case "osp1":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			case "lts":
+				switch ss[2] {
+				case "utopic", "vivid", "wily", "xenial":
+					return true
+				default:
+					return false
+				}
+			case "hwe":
+				switch ss[2] {
+				case "edge":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			case "xilinx":
+				return ss[2] == "zynqmp"
+			case "nvidia":
+				switch ss[2] {
+				case "tegra":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[2], 64)
+					return err == nil
+				}
+			default:
+				return false
+			}
+		case 4:
+			if ss[0] != "linux" {
+				return false
+			}
+			switch ss[1] {
+			case "azure":
+				if ss[2] != "fde" {
+					return false
+				}
+				_, err := strconv.ParseFloat(ss[3], 64)
+				return err == nil
+			case "intel":
+				if ss[2] != "iotg" {
+					return false
+				}
+				_, err := strconv.ParseFloat(ss[3], 64)
+				return err == nil
+			case "lowlatency":
+				if ss[2] != "hwe" {
+					return false
+				}
+				_, err := strconv.ParseFloat(ss[3], 64)
+				return err == nil
+			case "nvidia":
+				if ss[2] != "tegra" {
+					return false
+				}
+				switch ss[3] {
+				case "igx":
+					return true
+				default:
+					_, err := strconv.ParseFloat(ss[3], 64)
+					return err == nil
+				}
+			default:
+				return false
+			}
+		default:
+			return false
+		}
+	default:
+		return false
+	}
 }
